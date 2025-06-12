b534b59352b6c193fa736a10f08bcdee39a390f8
Akinola Adewole <adewoleakinola24@gmail.com>
2025-06-12 13:33:50 +0100

    react class continue

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 
 const Test = () => {
-  const Title = 'This is new React Class'
+  const Title = 'This is new React Class';
+  const link = 'https://www.facebook.com';
+  const user = {name:'Yoshi', age:30}
   return (
     <div>
         <div>I am test react</div>
         <div>React Class</div>
         <div>{ Title }</div>
         <a href="https://www.google.com">Google</a>
+        <a href={link}>Facebook</a>
         <div>{'Netninja, React class'}</div>
+        <div>{user.name} and {user.age}</div>
     </div>
   )
 }
