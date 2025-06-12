96ea623e71c179ce24528265375698271bfdb1c3
Akinola Adewole <adewoleakinola24@gmail.com>
2025-06-12 13:25:42 +0100

    react class

--- src/pages/Test.jsx
+++ src/pages/Test.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 
 const Test = () => {
+  const Title = 'This is new React Class'
   return (
     <div>
         <div>I am test react</div>
         <div>React Class</div>
+        <div>{ Title }</div>
+        <a href="https://www.google.com">Google</a>
+        <div>{'Netninja, React class'}</div>
     </div>
   )
 }
